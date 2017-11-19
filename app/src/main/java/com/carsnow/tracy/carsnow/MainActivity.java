4fc499eb6e944259fbcd9a3554d353e2209a4989
tracy <tracygipson066@gmail.com>
2017-11-19 17:40:56 +0530

    Initial commit

---
+++ app/src/main/java/com/carsnow/tracy/carsnow/MainActivity.java
@@ -0,0 +1,13 @@
+package com.carsnow.tracy.carsnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
