a8a498577e7cd613a4a6dc2c6c7820ea37a9dc27
Tracy Gipson <tracygipson@users.noreply.github.com>
2017-11-21 02:00:35 +0530

    Till i slept watching Stranger Things lol ;)

--- app/src/main/java/com/carsnow/tracy/carsnow/MainActivity.java
+++ app/src/main/java/com/carsnow/tracy/carsnow/MainActivity.java
@@ -1,10 +1,14 @@
 package com.carsnow.tracy.carsnow;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,11 +25,13 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     public String url= "https://raw.githubusercontent.com/tracygipson/CarsNow/master/cars.json";
     public Button btn;
-    public TextView txt;
+    public ImageView imageView;
+    public TextView txt,price,manu;
     public OkHttpClient client;
-    int count;
+    int count=1;
     JSONObject jobj;
     JSONArray jary;
+    String name = "",price1 = "",manu1 = "",img="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,29 +39,39 @@ public class MainActivity extends AppCompatActivity {
 
         btn = (Button) findViewById(R.id.nextbtn);
         txt = (TextView) findViewById(R.id.title);
+        price = (TextView) findViewById(R.id.price);
+        manu = (TextView) findViewById(R.id.manu);
+        imageView = (ImageView) findViewById(R.id.imageView);
         connectiony();
-
-
     btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            count = count + 1;
-            if (count == 1) {
-                firstdisplay(count);
+            if (count >= jary.length()){
+                try {
+                    getcardata(0);
+                    count = 1;
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
+            }
             else {
-                getcardata();
+                try {
+                    getcardata(count);
+                    count = count + 1;
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
-    });
-    }
-
+    }); }
     public void connectiony() {
         OkHttpClient client = new OkHttpClient();
         final Request request = new Request.Builder()
                 .url(url)
                 .build();
 
+        
         client.newCall(request).enqueue(new Callback(){
             @Override
             public void onFailure(Call call, IOException e) {
@@ -68,100 +84,42 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onResponse(Call call,final Response response) throws IOException {
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        String name = "";
                         try {
                             String data = response.body().string();
                             try {
                                 jobj = new JSONObject(data);
                                 jary = jobj.getJSONArray("cars");
-                                for(int i=0;i<jary.length();i++){
-                                    JSONObject jobj2 = jary.getJSONObject(i);
-                                    name = jobj2.getString("name");
-                                }
+                                JSONObject jobj2 = jary.getJSONObject(0);
+                                name = jobj2.getString("name");
+                                txt.setText(name);
+                                price1 = jobj2.getString("price");
+                                price.setText(price1);
+                                manu1 = jobj2.getString("manufacturer");
+                                manu.setText(manu1);
+                                img = jobj2.getString("img");
+                                Picasso.with(getApplicationContext()).load(img).resize(350, 220).into(imageView);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                             txt.setText(name);
                         } catch (IOException e) {
                             e.printStackTrace();
-                        }
-                    }
-                });
-            }
-
+                        }}
+                });}
         });
-
-
     }
-
-    public void firstdisplay(int count) {
-
-
+    public void getcardata(int count) throws JSONException {
+        JSONObject jobj2 = jary.getJSONObject(count);
+        name = jobj2.getString("name");
+        txt.setText(name);
+        price1 = jobj2.getString("price");
+        price.setText(price1);
+        manu1 = jobj2.getString("manufacturer");
+        manu.setText(manu1);
+        img = jobj2.getString("img");
+        Picasso.with(getApplicationContext()).load(img).resize(350, 220).into(imageView);
     }
-
-
-
-    public void getcardata(){
-        OkHttpClient client = new OkHttpClient();
-        final Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-            client.newCall(request).enqueue(new Callback(){
-            @Override
-            public void onFailure(Call call, IOException e) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        txt.setText("Failed");
-                    }
-                });
-            }
-
-
-
-
-            @Override
-            public void onResponse(Call call,final Response response) throws IOException {
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String name = "";
-                        try {
-                            String data = response.body().string();
-
-
-                            try {
-                                JSONObject jobj = new JSONObject(data);
-                                JSONArray jary = jobj.getJSONArray("cars");
-
-                                for(int i=0;i<jary.length();i++){
-                                    JSONObject jobj2 = jary.getJSONObject(i);
-                                    name = jobj2.getString("name");
-                                }
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                            txt.setText(name);
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-
-});
-
-}
-
-
-
 }
